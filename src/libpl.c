ec2ae072747a1f32ba2e29c60a6e3d9ceda590c2
David Young <davidy@passivelogic.com>
2023-10-20 12:56:19 -0400

    Documentation

--- src/libpl.c
+++ src/libpl.c
@@ -1,7 +1,16 @@
 
 /*
- * This is a needless library used to deceive Swift into allowing us to use getcontext() in
- * its basic, "returns once" form. This code is required just on Linux
+ * This library is used to create an "activity context" that attempts to mimic the Apple
+ * os.activity library's os_activity_get_identifier() function. In reality, there is no
+ * analog to Apple's library for Linux; rather, we're left with trying to create a stack-local
+ * unique ID, which is very platform-dependent.
+ *
+ * NB: The technique employed by this library is not reliable. It uses the libc getcontext()
+ * function to retrieve stack data, then logically ORs together the stack pointer and program counter,
+ * hoping to create a unique ID for any particular stack, regardless of thread. How successful we can
+ * be depends much on the definition of ucontext_t, and the information found in its mcontext_t struct.
+ * On ARM64, we get some good stuff and can make a reasonable attempt. It remains to be seen what other
+ * architectures, such as AMD64, give us.
  */
 
 #include "libpl.h"
