e1d594e2ed5fc70f5a8e50d448f81be5e84c6c68
David Young <davidy@passivelogic.com>
2023-10-23 16:45:43 -0400

    Use gcc built-in functions

--- include/libpl.h
+++ include/libpl.h
@@ -6,8 +6,6 @@
 #ifndef _LIBPL_H
 #define _LIBPL_H 1
 
-#include <ucontext.h>
-
 int getframep(unsigned long *current, unsigned long *parent);
 
 #endif  /* _LIBPL_H */
--- src/libpl.c
+++ src/libpl.c
@@ -14,7 +14,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 #include "libpl.h"
 
